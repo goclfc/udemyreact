62ddaa316132b4561f9de38621bde72e66edc1ed
GatsbyJr <gocha.berulava@gmail.com>
2022-03-03 23:16:25 +0400

    header
    update header js

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,8 +1,31 @@
 import React from 'react';
-
+import {AppBar,Toolbar,IconButton} from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
 const Header = () => {
     return (
-        <h1> Header</h1>
+        <AppBar 
+            position="fixed"
+            style={{
+                backgroundColor:"black",
+                padding:"10px"
+            }}
+        >
+            <Toolbar>
+                <div style={{display:'block'}}>
+                <div> Logo </div>
+                <div> Some text </div>
+                </div>
+                <IconButton
+                    aria-label="Menu"
+                    color="inherit"
+                    style={{
+                        marginLeft:'auto',
+                    }}
+                >
+                  <MenuIcon/>
+                </IconButton>
+            </Toolbar>
+        </AppBar>
     )
 }
 export default Header;
